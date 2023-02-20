780c4ed6738622aa4c84b048945fcfcd872a758f
abdishukri <abdishukrimohamed105@gmail.com>
2023-02-20 12:17:58 +0300

    added methods

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,36 @@
 require 'bundler'
 Bundler.require
 
-require_relative '../lib/student'
-
 DB = { conn: SQLite3::Database.new("db/school.db") }
 
+require_relative '../lib/student'
+
+
+
+
+
 # RUN CODE FROM HERE
+
+# create a table
+
+Student.create_table
+
+# student data
+shukri = Student.new(name: "shukri", age: 12)
+
+# insert record
+shukri.create
+
+# view all students
+Student.all
+
+# update student data
+shukri.age = 21
+shukri.update
+
+Student.all
+
+# DELETE 
+shukri.destroy
+
+Student.all
--- lib/student.rb
+++ lib/student.rb
@@ -4,18 +4,56 @@ class Student
     def initialize(name:, age:, id: nil)
         @id=id
         @name=name
-        @id=id
+        @age=age
     end
 
 # TODO: CREATE TABLE
+    def self.create_table
+        query = "CREATE TABLE IF NOT EXISTS students (name VARCHAR(255) NOT NULL, age INTEGER NOT NULL)"
+        DB[:conn].query(query)
+    end
 
 # TODO: INSERT RECORD
+   def create
+     sql = <<-SQL
+          INSERT INTO  students (name,age) values(?,?)
+     SQL
+    DB[:conn].query(query,self.name,self.age)
+   end
+
 
 # TODO: SHOW ALL RECORDS
 
+    def self.all
+        query = <<-SQL
+           SELECT * FROM students
+        SQL
+
+        DB[:conn].query(query).each do |row|
+            pp row
+        end
+    end
+
+
+
 # TODO: UPDATE RECORD
+    
+    def update
+        query = <<-SQL
+           UPDATE students SET name = ?, age = ?
+           WHERE name = ? 
+        SQL
+        DB[:conn].query(query, self.name, self.age, self.name)
+    end
 
 # TODO: DELETE RECORD
+
+    def destroy
+        query = <<-SQL
+            DELETE FROM students SET name = ?
+        SQL
+        DB[:conn].query(query, self.name)
+    end
 
 # TODO: CONVERT TABLE RECORD TO RUBY OBJECT
 
